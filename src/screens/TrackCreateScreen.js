e408afe886f81f41cbd9f1415f22e4b04de423ee
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-27 20:14:07 -0800

    Watch user position and print location to console

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet }  from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
-import { requestPermissionsAsync } from 'expo-location';
+import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
 import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
@@ -14,6 +14,13 @@ const TrackCreateScreen = () => {
       if (!granted) {
         throw new Error('Location permission not granted');
       }
+      await watchPositionAsync({
+        accuracy: Accuracy.BestForNavigation,
+        timeInterval: 1000,
+        distanceInterval: 10
+      }, (location) => {
+        console.log(location);
+      });
     } catch (e) {
       setErr(e);
     }
