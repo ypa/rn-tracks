a831cd81916bfb1cd4661c7599d08ab05ca751e6
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-28 15:23:42 -0800

    Stop tracking when user is not on TrackCreateScreen

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
 import { Accuracy, requestPermissionsAsync, watchPositionAsync } from 'expo-location';
 
-export default (callback) => {
+export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null);
+  const [subscriber, setSubscriber] = useState(null);
 
   const startWatching = async () => {
     try {
@@ -10,19 +11,25 @@ export default (callback) => {
       if (!granted) {
         throw new Error('Location permission not granted');
       }
-      await watchPositionAsync({
+      const sub = await watchPositionAsync({
         accuracy: Accuracy.BestForNavigation,
         timeInterval: 1000,
         distanceInterval: 10
       }, callback);
+      setSubscriber(sub);
     } catch (e) {
       setErr(e);
     }
   };
 
   useEffect(() => {
-    startWatching();
-  }, []);
+    if (shouldTrack) {
+      startWatching();
+    } else {
+      subscriber.remove();
+      setSubscriber(null);
+    }
+  }, [shouldTrack]);
 
   return [err];
 };
