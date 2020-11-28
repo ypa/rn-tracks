ef379e7ee3a6e552389540cdac8b0972c98988ed
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-28 14:18:50 -0800

    Update mock location starting coords

--- src/_mockLocation.js
+++ src/_mockLocation.js
@@ -11,8 +11,8 @@ const getLocation = increment => {
       accuracy: 5,
       altitudeAccuracy: 5,
       altitude: 5,
-      longitude: -122.0312186 + increment * tenMetersWithDegrees,
-      latitude: 37.33233141 + increment * tenMetersWithDegrees
+      latitude: 34.10072964483955 + increment * tenMetersWithDegrees,
+      longitude: -118.02252916072356 + increment * tenMetersWithDegrees
     }
   };
 };
