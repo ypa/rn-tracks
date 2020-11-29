cd6e211377bc143277bce57e9fb91d1a77e2f3bc
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-29 13:53:26 -0800

    Add TrackContext provider with minimal boiler plate and wire it up inside the App. Note: the order of wrapping the App component inside doesn't matter so wrapping at the outside most

---
+++ src/context/TrackContext.js
@@ -0,0 +1,21 @@
+import createDataContext from './createDataContext';
+
+const trackReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const fetchTracks = dispatch => () => {};
+
+const createTrack = dispatch => () => {};
+
+export const { Provider, Context } = createDataContext(
+  trackReducer,
+  {
+    fetchTracks: fetchTracks,
+    createTrack: createTrack
+  },
+  []
+);
