f001ddf4b9fca79c4ed66dd3180189100ac4d694
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-29 13:28:45 -0800

    Destructure recording off of state

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -9,9 +9,9 @@ import useLocation from '../hooks/useLocation';
 import TrackForm from '../components/TrackForm';
 
 const TrackCreateScreen = ({ isFocused }) => {
-  const { state, addLocation } = useContext(LocationContext);
-  const callback = useCallback((location) => { addLocation(location, state.recording) }, [state.recording]);
-  const [err] = useLocation(isFocused || state.recording, callback);
+  const { state: { recording }, addLocation } = useContext(LocationContext);
+  const callback = useCallback((location) => { addLocation(location, recording) }, [recording]);
+  const [err] = useLocation(isFocused || recording, callback);
 
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
