68ff35a3353751a7f9c6191977b112b005aea719
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-29 13:36:57 -0800

    Draw line on the map when the user start recording

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,7 +4,7 @@ import MapView, { Polyline, Circle} from 'react-native-maps';
 import { Context as LocationContext } from '../context/LocationContext';
 
 const Map = () => {
-  const { state: { currentLocation } } = useContext(LocationContext);
+  const { state: { currentLocation, locations } } = useContext(LocationContext);
 
   if (!currentLocation) {
     return <ActivityIndicator size="large" style={{ marginTop: 200 }} />;
@@ -24,6 +24,7 @@ const Map = () => {
       strokeColor="rgba(158, 158, 244, 1.0)"
       fillColor="rgba(158, 158, 244, 0.3)"
     />
+    <Polyline coordinates={locations.map(loc => loc.coords)} />
   </MapView>;
 };
 
