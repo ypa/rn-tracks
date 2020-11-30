dc969384f9b6f3f936e5f9f2ddebca59221d5e12
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-29 19:29:45 -0800

    Add navigation to TrackDetail Screen

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -16,7 +16,9 @@ const TrackListScreen = ({ navigation }) => {
         data={state}
         keyExtractor={item => item._id}
         renderItem={({ item }) => {
-          return <TouchableOpacity>
+          return <TouchableOpacity onPress={() =>
+            navigation.navigate('TrackDetail', { _id: item._id })
+          }>
             <ListItem chevron title={item.name} />
           </TouchableOpacity>
         }}
